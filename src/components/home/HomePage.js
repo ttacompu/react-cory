e66536eb01a860dc93f95e703e167a9959be4501
Aung <taung@cgsh.com>
2018-08-08 17:07:39 -0400

    first commit

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+class  HomePage extends Compoent{
+
+    render(){
+        return(
+            <div className="jumbotron">
+                <h1>Bootstrap Tutorial</h1> 
+            </div>
+        )
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import  "./components/home/HomePage";
