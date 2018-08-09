cddfd2caa036281a0fd4f278927082c55a7419b1
Aung <taung@cgsh.com>
2018-08-09 17:34:27 -0400

    good one

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,9 +1,32 @@
 import React, {Component} from 'react';
 
 class CoursesPage extends Component{
+    onTitleChanged = (event) =>{
+        const {course} = this.state;
+        course.title = event.target.value;
+        this.setState({course});
+        
+    }
+    onClickSave = (event) =>{
+        const {course} = this.state;
+        alert(course.title);
+    }
+        
+    state= {
+        course : { title : ''}
+    }
+    constructor(props, context){
+        super(props, context);
+    }
+
     render(){
         return (
-            <div>Courses</div>
+            <div>
+                <h1>Courses</h1>
+                <h2>Add Course</h2>
+                <input type="text" onChange={this.onTitleChanged} value={this.state.course.title} />
+                <input type="submit" value="Save" onClick={this.onClickSave}  />
+            </div>
         )
     }
 }
